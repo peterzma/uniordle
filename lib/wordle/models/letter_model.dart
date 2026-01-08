1bc62160e28c727c40a54f8422dd76a01b4b040c
peter <chuckaboxpeter@gmail.com>
2026-01-08 11:17:36 +1100

    feat: added word_model.dart

--- lib/wordle/models/letter_model.dart
+++ lib/wordle/models/letter_model.dart
@@ -16,7 +16,7 @@ class Letter extends Equatable {
 
   final LetterStatus status;
 
-  // background colour based on letter status
+    // background colour based on letter status
   Color get backgroundColor {
     switch (status) {
       case LetterStatus.initial:
@@ -30,7 +30,7 @@ class Letter extends Equatable {
     }
   }
 
-  Color get borderColor {
+    Color get borderColor {
     switch (status) {
       case LetterStatus.initial:
         return Colors.grey;
