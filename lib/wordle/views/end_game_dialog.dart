7d26826adf9911a90970e46297661e8b47cd729b
peter <chuckaboxpeter@gmail.com>
2026-01-13 14:11:50 +1100

    feat: added constructors

--- lib/wordle/views/end_game_dialog.dart
+++ lib/wordle/views/end_game_dialog.dart
@@ -1,1 +1,17 @@
 import 'package:flutter/material.dart';
+
+class EndGameDialog extends StatelessWidget {
+  final bool won;
+  final String solution;
+  final int attempts;
+  final VoidCallback onRestart;
+
+  const EndGameDialog({
+    required this.won,
+    required this.solution,
+    required this.attempts,
+    required this.onRestart,
+  });
+
+  @override
+}
