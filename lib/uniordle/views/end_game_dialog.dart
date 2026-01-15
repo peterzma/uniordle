ab368247025d05b6e777b4d9388e1b6e7956c31e
peter <chuckaboxpeter@gmail.com>
2026-01-15 16:16:40 +1100

    refactor: moved stat box and action button to widget folders

--- lib/uniordle/views/end_game_dialog.dart
+++ lib/uniordle/views/end_game_dialog.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:uniordle/uniordle/views/widgets/stat_box.dart';
+import 'package:uniordle/uniordle/views/widgets/action_button.dart';
 
 const Color _topBarWinColor = Color(0xFF55B725);
 const Color _topBarLoseColor = Color(0xFFC62121);
@@ -106,14 +108,14 @@ class EndGameDialog extends StatelessWidget {
                 Row(
                   children: [
                     Expanded(
-                      child: _StatBox(
+                      child: StatBox(
                         title: 'THE WORD WAS',
                         value: solution,
                       ),
                     ),
                     const SizedBox(width: 16), // padding between two boxes
                     Expanded(
-                      child: _StatBox(
+                      child: StatBox(
                         title: 'ATTEMPTS', 
                         value: '$attempts',
                       ),
@@ -124,14 +126,14 @@ class EndGameDialog extends StatelessWidget {
                 // button
                 Row(
                   children: [
-                    _ActionButton(
+                    ActionButton(
                       label: 'Back', 
                       onPressed: () => Navigator.pop(context), 
                       color: Color(0xFF1E2021),
                       hasBorder: true,
                     ),
                     const SizedBox(width: 16),
-                    _ActionButton(
+                    ActionButton(
                       label: 'New Game',
                       onPressed: onRestart,
                       color: _nextGameButtonColour,
@@ -144,103 +146,5 @@ class EndGameDialog extends StatelessWidget {
         ],
       ),
     );
-  }
-}
-
-// two centre buttons
-class _StatBox extends StatelessWidget {
-  final String title;
-  final String value;
-
-  const _StatBox({
-    required this.title,
-    required this.value,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container( 
-      padding: const EdgeInsets.symmetric(
-        horizontal: 16,
-        vertical: 8,
-      ),
-      decoration: BoxDecoration(
-        color: const Color(0xFF1A1C1E),
-        borderRadius: BorderRadius.circular(8),
-        border: Border.all(color: const Color(0xFF303436),
-        ),
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text(
-            title,
-            style: const TextStyle(
-              color: Color(0xFF8E9599),
-              fontSize: 16,
-              fontFamily: 'dm-sans',
-              fontWeight: FontWeight.w500
-              ),
-            ),
-          const SizedBox(height: 8),
-          FittedBox(
-            fit: BoxFit.scaleDown,
-            child: Text(
-            value,
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 36,
-                fontFamily: 'clashdisplay',
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-// two bottom buttons
-class _ActionButton extends StatelessWidget {
-  final String label;
-  final VoidCallback onPressed;
-  final Color color;
-  final bool hasBorder;
-
-  const _ActionButton({
-    required this.label,
-    required this.onPressed,
-    required this.color,
-    this.hasBorder = false,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded( 
-      child: ElevatedButton(
-        onPressed: onPressed, 
-        style: ElevatedButton.styleFrom(
-          backgroundColor: color,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8),
-            side: hasBorder
-              ? const BorderSide(color: Color(0xFF303436), width: 1)
-              : BorderSide.none,
-          ),
-          elevation: 0,
-          padding: const EdgeInsets.symmetric(vertical: 20),
-        ),
-        child: Text(
-          label,
-          style: const TextStyle(
-            fontSize: 14,
-            color: Colors.white,
-            fontFamily: 'clashdisplay',
-            fontWeight: FontWeight.w400,
-          ),
-        )
-      ),
-    );  
   }
 }
