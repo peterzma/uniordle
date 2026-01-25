fbc0f3a18929c9acfe54457e9944627827bcf972
peter <chuckaboxpeter@gmail.com>
2026-01-25 17:29:58 +1100

    feat: added profile data

---
+++ lib/features/profile/models/profile_data.dart
@@ -0,0 +1,11 @@
+class UserStats {
+  final String streak;
+  final String solved;
+  final String level;
+
+  UserStats({
+    required this.streak, 
+    required this.solved, 
+    required this.level
+  });
+}
