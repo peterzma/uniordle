e620bb9ad3f4afcfd0bf6d92b4287c3b852426c7
peter <chuckaboxpeter@gmail.com>
2026-01-27 19:27:44 +1100

    feat: created level up dialog

--- lib/features/game/widgets/level_up/level_up_dialog.dart
+++ lib/features/game/widgets/level_up/level_up_dialog.dart
@@ -1,1 +1,23 @@
 import 'package:uniordle/shared/exports/end_game_exports.dart';
+
+class LevelUpDialog extends StatefulWidget {
+  final int startingLevel;
+  final double startingProgress;
+  final double gainedXP;
+  final VoidCallback onContinue;
+
+  const LevelUpDialog({
+    super.key,
+    required this.startingLevel,
+    required this.startingProgress,
+    required this.gainedXP,
+    required this.onContinue,
+  });
+
+  @override
+  State<LevelUpDialog> createState() => _LevelUpDialogState();
+}
+
+class _LevelUpDialogState extends State<LevelUpDialog> with SingleTickerProviderStateMixin {
+}
+
